8b26b26863a6d48e011519dd4442d0cc2c625dc9
Your Name <sjukic1@etf.unsa.ba>
2020-03-25 23:18:36 +0100

    Kreiran korisnicki interfejs

--- src/ba/unsa/etf/rs/tut4/Controller.java
+++ src/ba/unsa/etf/rs/tut4/Controller.java
@@ -1,4 +1,8 @@
 package ba.unsa.etf.rs.tut4;
 
+import javafx.event.ActionEvent;
+
 public class Controller {
+    public void dodajArtikle(ActionEvent actionEvent) {
+    }
 }
--- src/ba/unsa/etf/rs/tut4/Main.java
+++ src/ba/unsa/etf/rs/tut4/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setTitle("Kasa");
+        primaryStage.setScene(new Scene(root, 400, 500));
         primaryStage.show();
     }
 
