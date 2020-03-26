5199ee0768ee317f263429aea2807e3628502955
Your Name <sjukic1@etf.unsa.ba>
2020-03-26 12:18:12 +0100

    Dodan ispis u drugi textArea

--- src/ba/unsa/etf/rs/tut4/Controller.java
+++ src/ba/unsa/etf/rs/tut4/Controller.java
@@ -4,10 +4,10 @@ import javafx.event.ActionEvent;
 import javafx.scene.control.TextArea;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class Controller {
     public TextArea uzimaArtikle;
+    public TextArea ispisArtikala;
 
     public void dodajArtikle(ActionEvent actionEvent) {
         String sviArtikli = uzimaArtikle.getText();
@@ -16,7 +16,10 @@ public class Controller {
             listaArtikala.add(new Artikal(artikal));
         }
         Artikal.izbaciDuplikate(listaArtikala);
-        System.out.println(listaArtikala);
-        System.out.println(listaArtikala.size());
+
+        for(int i=0; i<listaArtikala.size(); i++){
+            ispisArtikala.appendText(listaArtikala.get(i).toString() + "\n");
+        }
+
     }
 }
