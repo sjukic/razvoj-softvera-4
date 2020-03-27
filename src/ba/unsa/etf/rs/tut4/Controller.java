049e469da8b5ffbebf996ab25a2eac6a1ab41182
Your Name <sjukic1@etf.unsa.ba>
2020-03-27 23:47:10 +0100

    dodana metoda za button

--- src/ba/unsa/etf/rs/tut4/Controller.java
+++ src/ba/unsa/etf/rs/tut4/Controller.java
@@ -1,13 +1,19 @@
 package ba.unsa.etf.rs.tut4;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
 import javafx.scene.control.TextArea;
-
 import java.util.ArrayList;
+
 
 public class Controller {
     public TextArea uzimaArtikle;
     public TextArea ispisArtikala;
+
+    public TextArea ispisAktuelnogRacuna;
+    public Spinner spinnerKolicina;
+    public ChoiceBox padajucaLista;
 
     public void dodajArtikle(ActionEvent actionEvent) {
         String sviArtikli = uzimaArtikle.getText();
@@ -21,5 +27,21 @@ public class Controller {
             ispisArtikala.appendText(listaArtikala.get(i).toString() + "\n");
         }
 
+        for(int i=0; i<listaArtikala.size(); i++){
+            padajucaLista.getItems().add(listaArtikala.get(i).getSifra());
+        }
+
+    }
+
+    public void dugmeArtikliRacun(ActionEvent actionEvent) {
+        String sviArtikli = uzimaArtikle.getText();
+        ArrayList<Artikal> listaArtikala = new ArrayList<>();
+        for (String artikal: sviArtikli.split("\n")) {
+            listaArtikala.add(new Artikal(artikal));
+        }
+        Artikal.izbaciDuplikate(listaArtikala);
+        String s =  new String();
+        s = padajucaLista.getValue().toString() + spinnerKolicina.getValue().toString();
+        System.out.println(s);
     }
 }
